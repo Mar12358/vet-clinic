e5f0854f94c26e354099e29f4dab37f733bd78fd
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-07-11 13:32:29 -0300

    Add queries

--- queries.sql
+++ queries.sql
@@ -2,4 +2,27 @@
 
 CREATE DATABASE vet_clinic;
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals
+where NAME LIKE '%mon';
+
+SELECT NAME FROM animals
+WHERE DATE_OF_BIRTH >= '2016-01-01' AND DATE_OF_BIRTH <= '2019-12-31';
+
+SELECT NAME FROM animals
+WHERE neutered = true AND ESCAPE_ATTEMPTS < 3;
+
+SELECT DATE_OF_BIRTH FROM animals
+WHERE NAME = 'Agumon' OR NAME = 'Pikachu';
+
+SELECT NAME, ESCAPE_ATTEMPTS FROM animals
+WHERE weight_kg > 10.5;
+
+SELECT * FROM animals
+WHERE NEUTERED = true;
+
+SELECT * FROM animals
+WHERE NAME != 'Gabumon';
+
+SELECT * FROM animals 
+WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
