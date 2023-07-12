2805f2f56537141a0bdf37b4e78d8dfd8136c5d6
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-07-12 18:35:50 -0300

    Insert data into the owners' and species' table

--- data.sql
+++ data.sql
@@ -34,3 +34,23 @@ VALUES ('Blossom', '1998-09-13', 3, true, 17 );
 
 INSERT INTO animals (NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG)
 VALUES ('Ditto', '2022-05-14', 4, true, 22 );
+
+
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age)
+VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age)
+VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age)
+VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age)
+VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age)
+VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES ('Pokemon');
+INSERT INTO species (name)
+VALUES ('Digimon');
