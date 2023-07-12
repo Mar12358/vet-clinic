0ba06e0f2bb193576228d8ac6556dce15ef04a4a
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-07-12 13:01:11 -0300

    Add transactions to queries.sql

--- queries.sql
+++ queries.sql
@@ -26,3 +26,19 @@ WHERE NAME != 'Gabumon';
 SELECT * FROM animals 
 WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'digimon' WHERE NAME LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE DATE_OF_BIRTH > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
