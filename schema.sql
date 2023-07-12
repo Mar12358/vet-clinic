1bf215ded0f9510f0dbbf4601f7e6650a9b3f25c
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-07-12 15:49:25 -0300

    Add owners and species tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,16 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD species varchar(250);
+
+CREATE TABLE owners(
+   ID int GENERATED ALWAYS AS IDENTITY,
+   age int,
+   full_name varchar(250),
+   PRIMARY KEY(ID)
+);
+
+CREATE TABLE species(
+   ID int GENERATED ALWAYS AS IDENTITY,
+   name varchar(100),
+   PRIMARY KEY(ID)
+);
